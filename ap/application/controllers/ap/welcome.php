4bfee9580e9d40e4ae10249923d472aebc12e86c
Dennis Ho <dho1@shaw.ca>
2013-02-02 23:06:02 -0800

    Accounts Payable

---
+++ ap/application/controllers/ap/welcome.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('APPPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * controllers/ap/welcome.php
+ *
+ * Entry page to the ap page.
+ *
+ * @author		Dennis Ho
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+
+    function index() {
+        $this->data['pagebody'] = "welcome";
+        $this->data['vendors'] = $this->vendors->getAll_array();
+        $this->render();        
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: application/controllers/welcome.php */
---
+++ ap/application/views/ap/add_form.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* views/ap/add_form.php
+*
+* Present the add form page body.
+*
+* @author		Dennis Ho
+* ------------------------------------------------------------------------
+*/
+
+$this->load->helper('form');
+echo form_open('/ap/add_vendor/{function}');
+
+echo form_label('Vendor ID','id');
+echo form_input('id','{id}');
+echo '<br />';
+
+echo form_label('Name','name');
+echo form_input('name','{name}');
+echo '<br />';
+
+echo form_label('Status','status');
+echo form_input('status','{status}');
+echo '<br />';
+
+echo form_submit('', 'Ok');
+
+echo form_close();
