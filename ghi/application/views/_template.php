c6b8d56ca7599c084cdef14d7dcd2d7cd32d9b41
Jesse Braham <jbraham92@gmail.com>
2013-02-03 13:33:30 -0800

    Update ghi/application/views/_template.php

--- ghi/application/views/_template.php
+++ ghi/application/views/_template.php
@@ -7,7 +7,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>{pagetitle}</title>
+        <title>GHI</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
